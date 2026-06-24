da0a28ceeced6168d8a527c39ab22ed54afcb1ea
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 09:47:19 +0545

    Data Input in Users POST API

--- my-app/app/api/users/route.ts
+++ my-app/app/api/users/route.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/db";
 
 export async function POST(req: NextRequest) {
-    console.log("POST request received");
     const user = await req.json();
+
+    await db.execute(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [user.username, user.email, user.password]
+    );
+
     return NextResponse.json(
         { message: "POST request received", user: user },
         { status: 200 }
