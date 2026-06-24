dccc49f98a57f21a424b2161dd72438456cb837b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 09:22:55 +0545

    POST API for users

--- my-app/app/api/users/route.ts
+++ my-app/app/api/users/route.ts
@@ -1,16 +1,10 @@
-import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-    const cookie_get = req.cookies.get("noice");
-
-    const cookie = await cookies();
-
-    cookie.set("noice", "user");
-    return Response.json({ message: "Hello, World!" });
-}
-
-export async function POST(request: Request) {
-    const { name } = await request.json();
-    return Response.json({ message: `Hello, ${name}!` });
+export async function POST(req: NextRequest) {
+    console.log("POST request received");
+    const user = await req.json();
+    return NextResponse.json(
+        { message: "POST request received", user: user },
+        { status: 200 }
+    );
 }
