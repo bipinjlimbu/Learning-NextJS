0d0b00781457c924859f66bafc9378a58c86d7df
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-24 10:10:49 +0545

    Try Catch Block Added in users POST API

--- my-app/app/api/users/route.ts
+++ my-app/app/api/users/route.ts
@@ -2,15 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db";
 
 export async function POST(req: NextRequest) {
-    const user = await req.json();
+    try {
+        const user = await req.json();
+        await db.execute(
+            "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+            [user.username, user.email, user.password]
+        );
 
-    await db.execute(
-        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-        [user.username, user.email, user.password]
-    );
-
-    return NextResponse.json(
-        { message: "POST request received", user: user },
-        { status: 200 }
-    );
+        return NextResponse.json(
+            { message: "POST request received", user: user },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error inserting user:", error);
+        return NextResponse.json(
+            { message: "Error inserting user", error: error },
+            { status: 500 }
+        );
+    }
 }
