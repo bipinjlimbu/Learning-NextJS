da5a4103aeba4ed0a0c4840bb69a915eb4791f5c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-26 09:10:36 +0545

    Register Done

---
+++ my-app/actions/userActions.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { createUserInDB } from "@/services/userServices";
+
+export async function registerUser(data: FormData) {
+    const username = data.get('username')?.toString();
+    const email = data.get('email')?.toString();
+    const password = data.get('password')?.toString();
+
+
+    if (!username || !email || !password) {
+        throw new Error('Missing required fields');
+    }
+
+    const newUser = {
+        username,
+        email,
+        password
+    };
+    //database call to create user
+    await createUserInDB(newUser);
+    console.log('Registering user:', { username, email, password });
+
+    // Return a success message or any other relevant information
+    return { message: 'User registered successfully' };
+}
---
+++ my-app/app/register/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { registerUser } from "@/actions/userActions";
+
+export default function RegisterPage() {
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const something = await registerUser(formData);
+        alert('User registered successfully!');
+        console.log('User registered successfully:', something);
+    }
+
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl font-bold mb-4">Register</h1>
+            <form className="w-full max-w-sm bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
